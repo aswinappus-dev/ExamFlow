6d656927a78b5dc7d03e8468c095cecdae820bfc
Aswin Appus <aswinappus100@gmail.com>
2025-10-04 13:21:35 +0530

    Co-authored-by: Ansiyakn <Ansiyakn@users.noreply.github.com>

---
+++ src/main/java/com/examflow/ExamFlowApplication.java
@@ -0,0 +1,13 @@
+package com.examflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examflow/ExamFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
